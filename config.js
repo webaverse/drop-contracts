0729c3ef699173878d7cb85f3e88746e998ae864
mohsinanees <mohsin.anees09@gmail.com>
2021-12-21 22:37:59 +0500

    deleted addresses

--- config.js
+++ config.js
@@ -11,9 +11,6 @@ module.exports = {
         external_signer_key: process.env.EXTERNAL_SIGNER_PRIVATE_KEY,
         infura_key: process.env.INFURA_KEY,
         etherscan_api_key: process.env.ETHERSCAN_API_KEY,
-        Webaverse_contract_address: "0xcb15fF358998817a3dF382A4117ef440F9E1eBc0",
-        ERC721_contract_address: "0x64fb265ADDC3F9d6Fb9A2043AAD407198657b7D4",
-        ERC20_contract_address: "0x7D673Ef0EF30bEEe95b6aceA74732b1326CAb316"
     },
     hardhat: {
         network: "hardhat",
@@ -25,14 +22,10 @@ module.exports = {
         network: "sidechain",
         chainId: 1338,
         priv_key: process.env.PRIVATE_KEY,
-        Webaverse_contract_address: "0xB97f9250e8164b1C5A19b233D744F02b6277cB70",
-        ERC721_contract_address: "0x0cE21A5Fa1E3c50AF683c20Ad49C36c6c01316Ea",
-        ERC20_contract_address: "0xc3AC15084dd2023F3AcC3aa182eFed8563f7EF6d"
     },
     maticmainnet: {
         network: "maticmainnet",
         chainId: 137,
         priv_key: process.env.PRIVATE_KEY,
-        contract_address: "0xACE9277dF1Ca282f6924a731389A5cA9bd91c204"
     }
 };
